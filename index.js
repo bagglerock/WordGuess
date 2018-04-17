d227a7372c4873896f37b1fbee063f3cbd2353d8
Oscar Villalta <bagglerock@gmail.com>
2018-04-17 13:17:12 -0400

    starting to do the main game part

---
+++ index.js
@@ -0,0 +1,20 @@
+var inquirer = require("inquirer");
+var Word = require("./word");
+
+var wordbank = [
+    "subaru",
+    "honda",
+    "toyota",
+    "nissan",
+    "mitsubishi"
+];
+
+var game = function(){
+    //do some stuff
+
+
+
+
+
+    game();
+}
